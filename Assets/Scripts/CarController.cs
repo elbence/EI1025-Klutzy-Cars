544de19d354046ea2b2cd6bf9f4edb02cb00b96a
gericm <erickmontes555@gmail.com>
2022-12-14 19:17:52 +0100

    Add collision detection to react to police hits

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -57,4 +57,15 @@ public class CarController : MonoBehaviour
         AxisHorizontal = Input.GetAxis("Horizontal");
         AxisVertical = Input.GetAxis("Vertical");
     }
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Hitted by police!");
+        }
+    }
 }
